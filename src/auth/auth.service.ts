ad77d962ab1d2604aca8af2f42e0c03f472834aa
Ramadhan Aheebwa <raaheebwa@gmail.com>
2022-07-10 19:50:11 +0300

    chore: can register/login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,48 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import * as argon from "argon2";
+import { AuthDto } from "./dto";
 
 @Injectable()
 export class AuthService {
     constructor(
         private readonly prisma: PrismaService,
     ) {}
-    login() {
-        return {msg: 'I ahve logged in'};
+    async login(dto: AuthDto) {
+        const user = await this.prisma.user.findFirst({
+            where: {
+                email: dto.email,
+            },
+        });
+
+        if (!user) {
+            throw new ForbiddenException("Invalid credentials");
+        }
+
+        const isValid = await argon.verify(user.hash, dto.password);
+        if (!isValid) {
+            throw new ForbiddenException("Invalid credentials");
+        }
+
+        return user;
     }
-    signup() {
-        return {msg: 'I have signed up'};
+    async signup(dto: AuthDto) {
+        // generate password hash
+        const hash = await argon.hash(dto.password);
+        // save new user to db
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+            });
+            return user;
+        }
+        catch (err) {
+            if (err.code === 'P2002') {
+                throw new ForbiddenException('Credentials are taken');
+            }
+        }
     }
 }
